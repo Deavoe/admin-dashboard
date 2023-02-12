45e1065d1bc24b00761b27779a92416d056452a6
Deavoe <123446640+Deavoe@users.noreply.github.com>
2023-02-11 19:43:25 -0500

    Prepare announcements and a randomized trending section

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+function getLightnessOfRGB(rgbString) {
+    const rgbIntArray = (rgbString.replace(/ /g, '').slice(4, -1).split(',').map(e => parseInt(e)));
+    const highest = Math.max(...rgbIntArray);
+    const lowest = Math.min(...rgbIntArray);
+    return (highest + lowest) / 2 / 255;
+  }
+
+function RandomColor(icon) {
+    icon.style.backgroundColor = getRandomColor()
+    if (getLightnessOfRGB(icon.style.backgroundColor) < .31 ) { // Invert avatar if the bg color is too dark
+        let tc = icon.querySelector('.p-icon')
+        tc.classList.add("invert")
+    }
+}
+
+// Create and display 12 projects
 let projects = 12
 let pc = document.querySelector('.project-container')
 var div = document.querySelector('.project')
@@ -6,6 +31,29 @@ for (let i = 1; i<projects; i++) {
     pc.appendChild(clone);
 }
 
+// Create and display a random number of randomized trending projects
+let profiles = Math.floor(Math.random() * 25);
+let tc = document.querySelector('.trending-projects')
+var di = document.querySelector('.t-profile')
+
+let username = di.querySelector(".username")
+username.textContent = "@username" + Math.floor(Math.random() * 99999);
+
+let icon = di.querySelector(".avatar")
+RandomColor(icon)
+
+for (let i = 1; i<projects; i++) {
+    clone = di.cloneNode(true); // true = clone all childNodes and event handlers
+    let username = clone.querySelector(".username")
+    username.textContent = "@username" + Math.floor(Math.random() * 99999)
+
+    let icon = clone.querySelector(".avatar")
+    RandomColor(icon)
+
+    tc.appendChild(clone);
+}
+
+// Force some hover animations
 function simHover(e) {
     e.target.classList.toggle("sb-hover");
     // e.target.parentElement.querySelector([type='a']).classList.toggle("sb-hover")
